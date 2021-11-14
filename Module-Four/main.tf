17c43cbdc2a689c9239f439f3d9f6f9f3cdf1348
Chris Krieger <ckriege2@emich.edu>
2021-11-13 19:05:33 -0500

    Module-Four Code

--- Module-Four/main.tf
+++ Module-Four/main.tf
@@ -13,8 +13,7 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-resource "azurerm_resource_group" "IT356_TF_Test_RG" {
-  name     = "IT356_TF_Test_RG"
+resource "azurerm_resource_group" "RG_1" {
   location = "eastus2"
 }
 
@@ -22,12 +21,12 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "IT356_TF_Test_VNET"
   address_space       = ["10.0.0.0/16"]
   location            = "eastus2"
-  resource_group_name = "IT356_TF_Test_RG"
+  resource_group_name = azurerm_resource_group.RG_1.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "IT365-SN-Windows"
-  resource_group_name  = "IT356_TF_Test_RG"
+  name = "IT365-SN-Windows"
+  resource_group_name  = azurerm_resource_group.RG_1.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-}
+  }
